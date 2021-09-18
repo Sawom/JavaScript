1d8f0e070d1475652bc30763ea5c6948e9329695
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:50:56 +0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+var num = 69;
+var num2 = 69.99;
+var stringType = 'sawom'
+var bl = true;
+console.log("num 1 is int = ",num);
+console.log("num 2 is float = ",num2);
+console.log("this is string = ",stringType);
+console.log("this is bolean = ",bl);

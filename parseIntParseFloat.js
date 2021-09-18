5a0df37e3179e91880d111fb3cff873c42dbbed8
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:50:30 +0600

    Add files via upload

---
+++ parseIntParseFloat.js
@@ -0,0 +1,19 @@
+var num1 = '36';
+var num2 = '38' ;
+
+var result = num1+num2;
+console.log(result);
+console.log("apply parse int : ");
+var num3 = parseInt(num1);
+var num4 = parseInt(num2);
+var result2 = num3+num4;
+console.log(result2);
+
+console.log("apply parse float : ");
+var num5 = '36.69';
+var num6 = '40.99' ;
+
+var num7 = parseFloat(num5);
+var num8 = parseFloat(num6);
+var result3 = num7+num8;
+console.log(result3);

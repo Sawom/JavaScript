39bfc62920125e7aa23ab28c13fec321463f6b8d
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:50:20 +0600

    Add files via upload

---
+++ mathOperation.js
@@ -0,0 +1,13 @@
+var num1 = 99;
+var num2 = 69;
+var add = num1+num2;
+var sub = num1-num2;
+var mul = num1*num2;
+var div = num1/num2;
+var modulus = num1%num2;
+
+console.log("addition = ",add);
+console.log("substruction = ",sub);
+console.log("multiplication = ",mul);
+console.log("division = ",div);
+console.log("modulus = ",modulus);

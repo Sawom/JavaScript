390a4d7af98e5a7e45423c04c080a7092ef517f9
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-19 03:49:38 +0600

    Add files via upload

---
+++ grade.js
@@ -0,0 +1,14 @@
+var num= 89;
+if(num>80){
+    console.log("A+");
+}else if(num>70){
+    console.log("A");
+}else if(num>60){
+    console.log("B");
+}else if(num>50){
+    console.log("C");
+}else if(num>40){
+    console.log("D");
+}else{
+    console.log("fail!");
+}

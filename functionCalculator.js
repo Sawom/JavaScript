1c225dc92308651a969430edf212688d3861f617
Abdur Rashid Sawom <asawom250@gmail.com>
2021-09-21 16:41:08 +0600

    Add files via upload

---
+++ functionCalculator.js
@@ -0,0 +1,30 @@
+var num1 = 69 ;
+var num2 = 38 ;
+
+function add(num1,num2){
+    aresult = num1+num2;
+    return aresult;
+}
+resultadd = add(num1,num2);
+console.log("addition = ",resultadd);
+
+function sub(num1,num2){
+    subresult = num1-num2;
+    return subresult;
+}
+resultsub = sub(num1,num2);
+console.log("substruction = ",resultsub);
+
+function mul(num1,num2){
+    mresult = num1*num2;
+    return mresult;
+}
+resultm = mul(num1,num2);
+console.log("multiplication = ",resultm);
+
+function div(num1,num2){
+    dresult = num1/num2;
+    return dresult;
+}
+resultdiv = div(num1,num2);
+console.log("multiplication = ",resultdiv);

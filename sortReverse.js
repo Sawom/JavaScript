e8db12e8e45db02058225d1391fb1fb123764072
Abdur Rashid Sawom <64780532+Sawom@users.noreply.github.com>
2021-12-17 19:49:06 +0600

    Add files via upload

---
+++ sortReverse.js
@@ -0,0 +1,11 @@
+var numbers = [1,2,3,4,5,6,7,8,10,56,258,9,14,52];
+const numbers2 = numbers.sort();
+console.log(numbers2);
+const friend = ['mahi', 'afjal', 'arannya','fahim','erin'];
+const friend2 = friend.sort();
+console.log(friend2);
+// reverse
+const friend3 = friend.reverse();
+console.log(friend3);
+const friend4 = friend.sort().reverse();
+console.log(friend4);
